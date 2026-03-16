af8e0cabe4ae550e5d674989b698739da8a26fed
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-17 03:43:07 +0530

    feat: integrate src/index.js

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const metricsController = require('./controllers/metrics');
+
+app.use('/api/metrics', metricsController);
+
+app.get('/', (req, res) => {
+    res.send('Aurora Insights API');
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
